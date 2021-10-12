a94ec082a45b10b6956c56ba8697f913a1f62389
Cássia Lima <cassiadosreislima@gmail.com>
2021-10-12 19:13:28 -0300

    testes unitarios

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
@@ -7,5 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	public Optional <Usuario> findByUsuario(String usuario);
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String string);
+
+	public Usuario findByNome(String string);
 	
 }
